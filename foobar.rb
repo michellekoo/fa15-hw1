84fced0deb8af4d4d781d8f5d4231f6d16e457f4
Michelle <michellevkoo@yahoo.com>
2015-09-12 08:55:58 -0700

    Finished ruby on rails hw1

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,19 @@
 class Foobar
   # Q4 CODE HERE
+  require 'set'
+  def self.baz(arr)
+  	arr2 = []
+    for item in arr
+    	temp = Integer(item) + 2
+    	if (temp % 2 == 0) and (temp < 10)
+    		arr2.push(temp)
+    	end
+    end 
+    s = arr2.to_set
+    sum = 0
+    for item in s
+    	sum += item
+    end
+    sum
+  end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,13 +1,16 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  (a + b) ** 2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  (a.map! {|item| item + 1}).sort
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+  "#{first_name} #{last_name}"
 end
 
 def blockin_time(a)
